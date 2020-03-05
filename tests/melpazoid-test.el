ea25f2a9dc45ae813e0df001570309e6b49bd7b8
conao3 <conao3@gmail.com>
2020-03-05 17:49:45 +0900

    add testcases for ported function from MELPA

--- tests/melpazoid-test.el
+++ tests/melpazoid-test.el
@@ -27,9 +27,54 @@
 (require 'buttercup)
 (require 'melpazoid)
 
+(defconst melpazoid-directory (file-name-directory
+                               (directory-file-name
+                                (file-name-directory
+                                 (or byte-compile-current-file
+                                     load-file-name
+                                     (buffer-file-name)))))
+  "Path to melpazoid root.")
+
 (describe "A suite"
   (it "contains a spec with an expectation"
       (expect t :to-be t)))
+
+(describe "Function ported from MELPA package-build"
+  (it "config-file-list"
+    (expect
+     (melpazoid-build--config-file-list
+      '(melpazoid :fetcher github :repo "riscy/melpazoid"
+                  :files (:defaults (:exclude "melpazoid-async.el"))))
+     :to-equal
+     '("*.el" "*.el.in" "dir"
+       "*.info" "*.texi" "*.texinfo"
+       "doc/dir" "doc/*.info" "doc/*.texi" "doc/*.texinfo"
+       (:exclude ".dir-locals.el" "test.el" "tests.el" "*-test.el" "*-tests.el")
+       (:exclude "melpazoid-async.el")))
+
+    (expect
+     (melpazoid-build--config-file-list
+      '(melpazoid-async :fetcher github :repo "riscy/melpazoid"
+                        :files ("melpazoid-async.el")))
+     :to-equal
+     '("melpazoid-async.el")))
+
+  (it "expand-source-file-list"
+    (expect
+     (melpazoid-build--expand-source-file-list
+      '(melpazoid :fetcher github :repo "riscy/melpazoid"
+                  :files (:defaults (:exclude "melpazoid-async.el")))
+      melpazoid-directory)
+     :to-equal
+     '("melpazoid-build.el" "melpazoid-checker.el" "melpazoid.el"))
+
+    (expect
+     (melpazoid-build--expand-source-file-list
+      '(melpazoid-async :fetcher github :repo "riscy/melpazoid"
+                        :files ("melpazoid-async.el"))
+      melpazoid-directory)
+     :to-equal
+     '("melpazoid-async.el"))))
 
 ;; (provide 'melpazoid-test)
 ;;; melpazoid-test.el ends here
