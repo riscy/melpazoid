fee1328a5ca5d9a6e905a6858095c3e145e76ebb
conao3 <conao3@gmail.com>
2020-03-05 18:53:19 +0900

    add melpazoid-file.el

---
+++ melpazoid-file.el
@@ -0,0 +1,30 @@
+;;; melpazoid-file.el --- Manage tool for .melpazoid  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Naoya Yamashita
+
+;; Author: Naoya Yamashita <conao3@gmail.com>
+;; URL: https://github.com/riscy/melpazoid
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Manage tool for .melpazoid.
+
+;;; Code:
+
+
+
+(provide 'melpazoid-file)
+;;; melpazoid-file.el ends here
